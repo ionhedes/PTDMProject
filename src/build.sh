ea69944df0ff0afb5f1c15b4fd9c9fb898d5a6a7
Ioan Hedes <ihedes13@gmail.com>
2020-12-26 20:12:39 +0200

    Added some libraries necessary form compiling the code.

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Building main file..."
-g++ -Wall -o main main.cpp -I /usr/local/include -L /usr/local/bin -lopencv_videoio -lopencv_core -lopencv_highgui
+g++ -Wall -o main main.cpp -I /usr/local/include -L /usr/local/bin -lopencv_videoio -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_objdetect -lopencv_face
 echo "Done;"
